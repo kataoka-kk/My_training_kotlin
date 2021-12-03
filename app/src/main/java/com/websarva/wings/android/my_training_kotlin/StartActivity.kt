ffd0cd030448f0e66c3d52d0ea831edd46ed2ea6
kataoka-kk <horeizai.1201@gmail.com>
2021-12-03 12:13:17 +0900

    Thema決め

--- app/src/main/java/com/websarva/wings/android/my_training_kotlin/StartActivity.kt
+++ app/src/main/java/com/websarva/wings/android/my_training_kotlin/StartActivity.kt
@@ -1,11 +1,14 @@
 package com.websarva.wings.android.my_training_kotlin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
+
+        var lotteryButton: Button = findViewById(R.id.move_lottery_button)
     }
 }
---
+++ app/src/main/java/com/websarva/wings/android/my_training_kotlin/screens/LotteryActivity.kt
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.my_training_kotlin.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.websarva.wings.android.my_training_kotlin.R
+
+
+class LotteryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lottery)
+    }
+}
